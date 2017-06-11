90b6460e41973a78b1338f924adc8f3b67bff3c4
Kronan <tobuything@gmail.com>
2017-06-11 22:25:04 +0300

    Test3

---
+++ PomodoroTech/View/TasksViewCell.m
@@ -0,0 +1,29 @@
+//
+//  TasksViewCell.m
+//  PomodoroTech
+//
+//  Created by Kronan on 4/28/17.
+//  Copyright © 2017 Kronan. All rights reserved.
+//
+
+#import "TasksViewCell.h"
+
+@interface TasksViewCell()
+
+@end
+
+
+@implementation TasksViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
